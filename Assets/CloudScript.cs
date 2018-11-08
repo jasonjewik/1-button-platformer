dc4b3b6722258cb428ca64ff0ee012ae04d56ae9
Jason Jewik <jj11d7t@gmail.com>
2018-11-08 10:54:21 -0800

    Clouds move

--- Assets/CloudScript.cs
+++ Assets/CloudScript.cs
@@ -6,20 +6,30 @@ public class CloudScript : MonoBehaviour {
 
     private Transform cloudTF;
     private Vector3 pos;
-    public float speed = 1.0f;
+    private float speed;
+    private float leftEdge = -16f;
 
     public GameObject player;
 
     private void Start()
     {
+        speed = Random.Range(0.2f, 0.7f); ;
+        cloudTF = GetComponent<Transform>();
         player = GameObject.FindGameObjectWithTag("Player");   
     }
 
     // Update is called once per frame
     void Update () {
+        pos = cloudTF.position;
+
         if (player.GetComponent<PlayerScript>().getGameState())
         {
             cloudTF.position = new Vector2(pos.x - speed * Time.deltaTime, pos.y);
+        }
+
+        if (pos.x < leftEdge)
+        {
+            GameObject.Destroy(this.gameObject);
         }
     }
 }
--- Assets/CreateCloudsScript.cs
+++ Assets/CreateCloudsScript.cs
@@ -10,14 +10,15 @@ public class CreateCloudsScript : MonoBehaviour {
     // Use this for initialization
     void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
-        InvokeRepeating("CreateClouds", 0, 5.0f);
+        InvokeRepeating("CreateClouds", 0, 10.0f);
     }
 
     void CreateClouds()
     {
+        Vector3 pos = this.transform.position;
         if (player.GetComponent<PlayerScript>().getGameState())
         {
-            Instantiate(cloud, this.transform.position, new Quaternion());
+            Instantiate(cloud, new Vector2(pos.x, pos.y + Random.Range(-1f, 2.5f)), new Quaternion());
         }
     }
 }
